05361143ed542b3c4fb1af916b9af1b47896fc29
seromaho <seromaho@t-online.de>
2023-12-03 07:10:49 +0100

    Fix Post-Deployment SQL Script for inserting table data in { ~ / DatabaseProject.WaWi / Scripts }

--- DatabaseProject.WaWi/Scripts/Script.Data.sql
+++ DatabaseProject.WaWi/Scripts/Script.Data.sql
@@ -1,14 +1,14 @@
 -- This file contains SQL statements that will be executed after the build script.
 
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_Abteilungen' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_Abteilungen]
-FROM './Data/Data.Abteilungen.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.Abteilungen.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -17,14 +17,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_Anreden' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_Anreden]
-FROM './Data/Data.Anreden.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.Anreden.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -33,14 +33,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_Artikel' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_Artikel]
-FROM './Data/Data.Artikel.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.Artikel.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -49,14 +49,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_ArtikelGruppen' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_ArtikelGruppen]
-FROM './Data/Data.ArtikelGruppen.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.ArtikelGruppen.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -65,14 +65,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_Bestellungen' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_Bestellungen]
-FROM './Data/Data.Bestellungen.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.Bestellungen.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -81,14 +81,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_BestellungenPositionen' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_BestellungenPositionen]
-FROM './Data/Data.BestellungenPositionen.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.BestellungenPositionen.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -97,14 +97,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_GruppenMerge1' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_GruppenMerge1]
-FROM './Data/Data.GruppenMerge1.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.GruppenMerge1.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -113,14 +113,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_GruppenMerge2' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_GruppenMerge2]
-FROM './Data/Data.GruppenMerge2.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.GruppenMerge2.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -129,14 +129,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_Interessen' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_Interessen]
-FROM './Data/Data.Interessen.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.Interessen.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -145,14 +145,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_Kunden' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_Kunden]
-FROM './Data/Data.Kunden.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.Kunden.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -161,14 +161,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_KundenInteressen' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_KundenInteressen]
-FROM './Data/Data.KundenInteressen.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.KundenInteressen.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -177,14 +177,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_Lager' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_Lager]
-FROM './Data/Data.Lager.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.Lager.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -193,14 +193,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_Lagerstand' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_Lagerstand]
-FROM './Data/Data.Lagerstand.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.Lagerstand.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -209,14 +209,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_Lieferanten' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_Lieferanten]
-FROM './Data/Data.Lieferanten.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.Lieferanten.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -225,14 +225,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_Personal' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_Personal]
-FROM './Data/Data.Personal.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.Personal.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -241,14 +241,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_Setartikel' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_Setartikel]
-FROM './Data/Data.Setartikel.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.Setartikel.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -257,14 +257,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_Status' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_Status]
-FROM './Data/Data.Status.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.Status.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -273,14 +273,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_Testartikel' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_Testartikel]
-FROM './Data/Data.Testartikel.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.Testartikel.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -289,14 +289,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_Testpersonal' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_Testpersonal]
-FROM './Data/Data.Testpersonal.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.Testpersonal.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -305,14 +305,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_Titel' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_Titel]
-FROM './Data/Data.Titel.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.Titel.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -321,14 +321,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_Wareneingang' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_Wareneingang]
-FROM './Data/Data.Wareneingang.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.Wareneingang.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
@@ -337,14 +337,14 @@ WITH
 )
 GO
 
--- Connect to the 'WaWi_DB' database to run this snippet
-USE [WaWi_DB]
+-- Connect to the 'DB_WaWi' database to run this snippet
+USE [DB_WaWi]
 -- Import table data into table 'TBL_WareneingangPositionen' from the specified file
 -- Skip the header (first row)
 -- Use a comma (,) as the field terminator
 -- Use the default row terminator: the newline character (\n)
 BULK INSERT [TBL_WareneingangPositionen]
-FROM './Data/Data.WareneingangPositionen.csv'
+FROM 'DatabaseProject.WaWi\Data\Data.WareneingangPositionen.csv'
 WITH
 (
 	-- CODEPAGE = 'ACP',
