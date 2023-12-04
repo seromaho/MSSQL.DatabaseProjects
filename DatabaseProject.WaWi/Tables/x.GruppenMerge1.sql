0fc6ab22298457adb08fdde16762f7dedaa250c0
Sebastian Höft <sebastian.hoeft@multimedial-systeme.de>
2023-12-04 05:58:31 +0100

    Update { SQLPROJ } in { ~ / DatabaseProject.WaWi } to target { .NET Framework } version { v4.7.2 }

--- DatabaseProject.WaWi/Tables/x.GruppenMerge1.sql
+++ DatabaseProject.WaWi/Tables/x.GruppenMerge1.sql
@@ -1,4 +1,4 @@
-CREATE TABLE [dbo].[xGruppenMerge1] (
+CREATE TABLE [dbo].[x_GruppenMerge1] (
     [ArtGr]     CHAR (2)     NOT NULL,
     [ArtGrText] VARCHAR (50) NOT NULL
 );
--- DatabaseProject.WaWi/Tables/x.GruppenMerge2.sql
+++ DatabaseProject.WaWi/Tables/x.GruppenMerge2.sql
@@ -1,4 +1,4 @@
-CREATE TABLE [dbo].[xGruppenMerge2] (
+CREATE TABLE [dbo].[x_GruppenMerge2] (
     [ArtGr]     CHAR (2)     NOT NULL,
     [ArtGrText] VARCHAR (50) NOT NULL
 );
